1c8734204e7dc732ac304abc3e0937c07e982a3b
Johnathan Mueller <Mainman002@users.noreply.github.com>
2023-10-10 11:08:50 -0500

    main.rs Code Cleanup
    Removed code comments in main.rs that were no longer needed

--- src/main.rs
+++ src/main.rs
@@ -52,39 +52,6 @@ fn main() {
             AmbientLightPlugin,
             FloorPlugin,
             TiefighterPlugin))
-        // .add_systems(
-        //    Startup,
-        //    (
-                // spawn_debug_text,))
-        // .add_systems(Update, _debug)
     .run();
 }
-
-
-// #####################################################################
-// Object Spawn Functions
-// #####################################################################
-
-
-// Debug Text
-// fn spawn_debug_text(
-//     mut commands: Commands,
-// ) {
-//     let debug_text =
-//         TextBundle::from_section(
-//             "A simple scene with a Tiefighter and PBR rendering\n\
-//             Gizmos are manually placed and sized currently\n\
-//             All materials were default in GLTF export / import",
-//             TextStyle {
-//                 font_size: 20.,
-//                 ..default()
-//             })
-//         .with_style(Style {
-//             position_type: PositionType::Absolute,
-//             top: Val::Px(12.0),
-//             left: Val::Px(12.0),
-//             ..default()
-//         });
-//     commands.spawn(debug_text);
-// }
 
