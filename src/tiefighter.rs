0b60bb5bdd2adc48ca50a5cea61524dc21772684
Johnathan Mueller <Mainman002@users.noreply.github.com>
2023-10-11 08:20:40 -0500

    Tiefighter ECS Rotation
    Tiefighter rotates counter clockwise to test ECS

--- src/tiefighter.rs
+++ src/tiefighter.rs
@@ -1,41 +1,39 @@
 use bevy::prelude::*;
+use std::default::Default;
 
 pub struct TiefighterPlugin;
 
 impl Plugin for TiefighterPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems( 
-            Startup,
-            spawn_tiefighter);
-        app.add_systems(
-            Update, 
-            (
-                _debug,
-        ));
+        app.add_systems(Startup, setup);
+        app.add_systems(Update, rotate_tiefighter);
     }
 }
 
+#[derive(Resource)]
+struct Tiefighter(Entity);
 
-fn spawn_tiefighter(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>
-) {
-    let tiefighter = SceneBundle {
-        scene: asset_server.load("models/TieFighter.gltf#Scene0"),
-        transform: Transform::from_xyz(0.0, 1.2, 0.0),
-        ..default()
-    };
-    commands.spawn(tiefighter);
+// Load GLTF Model Into Scene
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let tiefighter_entity = commands
+        .spawn(SceneBundle {
+            scene: asset_server.load("models/TieFighter.gltf#Scene0"),
+            transform: Transform::from_xyz(0.0, 1.2, 0.0),
+            ..Default::default()
+        })
+        .insert(GlobalTransform::default())
+        .id(); // Get the entity ID
+
+    // Create the Tiefighter resource with the associated entity
+    commands.insert_resource(Tiefighter(tiefighter_entity));
 }
 
-
-fn _debug (
-    mut gizmos: Gizmos, 
-    _time: Res<Time>
-) {
-    gizmos.cuboid(
-        Transform::from_xyz(0.0, 1.2, 0.0).with_scale(Vec3::new(2.08, 2.4, 2.0)),
-        Color::GREEN,
-    );
+// Rotate Tiefighter Entity
+fn rotate_tiefighter(mut query: Query<&mut Transform>, tiefighter: Res<Tiefighter>, time: Res<Time>) {
+    if let Ok(mut transform) = query.get_component_mut::<Transform>(tiefighter.0) {
+        transform.rotate_around(
+            Vec3::ZERO,
+            Quat::from_axis_angle(Vec3::Y, -8f32.to_radians() * time.delta_seconds()),
+        );
+    }
 }
-
